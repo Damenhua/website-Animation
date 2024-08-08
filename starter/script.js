bdc46c797aaa8c468dbbac7c405279dbfcd9e468
Damenhua <154488852+Damenhua@users.noreply.github.com>
2024-08-08 23:51:59 +0800

    navigation

--- starter/script.js
+++ starter/script.js
@@ -5,22 +5,45 @@
 
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
+// btn
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
+const learnBtn = document.querySelector('.btn--scroll-to');
+// section
+const section1 = document.querySelector('#section--1');
+const sections = document.querySelectorAll('.nav__links');
 
+// openModal function
 const openModal = function () {
   modal.classList.remove('hidden');
   overlay.classList.remove('hidden');
 };
 
+// closeModal function
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
 
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
+// open account modal
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
 
+// Learn more btn
+learnBtn.addEventListener('click', function (e) {
+  section1.scrollIntoView({ behavior: 'smooth' });
+});
+
+// navigation, do not use forEach
+const navsHandler = function (e) {
+  e.preventDefault();
+
+  if (e.target.classList.contains('nav__link')) {
+    const hash = e.target.getAttribute('href');
+    document.querySelector(hash).scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
+sections.addEventListener('click', navsHandler);
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
